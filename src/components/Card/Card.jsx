255d30b45108dc0389d55ba35711fa760c1d5a59
zimoscop <zimoscop@gmail.com>
2023-03-01 16:51:37 +0300

    feat:likes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+// import like from "../components/image/Group 1 like";
+// import notlike from "../components/image/Group 1-not";
 
 export const Card = (props) => {
   const { id, name, actor, gender, wand, house, image, alive } = props;
+
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
   return (
     <div id={id} className="hp__card">
       <div className="hp__pic">
         {name} - foto not found
-        <img src={image} alt="foto" />
+        <img src={image} alt="" />
+        <button
+          data-name={name}
+          onClick={handleClick}
+          className={active ? "like " : "notlike"}
+        ></button>
       </div>
       <div className="hp__text">
         <h2>{name}</h2>
